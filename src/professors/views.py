bb031a9b71c69e6d2070349bc55fd6e167977610
teja <tejavarma.developer@gmail.com>
2019-07-08 13:47:34 +0530

    Add dynamic content to template

--- src/professors/views.py
+++ src/professors/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def professors(request, *args, **kwargs):
+    test = {
+        'name':'Teja',
+        'branch':'CSE',
+        'city':'Vizag',
+        'area':'Seethampeta'
+    }
     # return HttpResponse("<h1>This is professors page</h1>")
-    return render(request,"prof.html",{})
+    return render(request,"prof.html",test)
